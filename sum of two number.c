d7f5e0f80b03553641801325f953ba2bb0121629
19P31A0313 <93141036+19P31A0313@users.noreply.github.com>
2021-10-30 12:11:55 +0530

    Add files via upload

---
+++ sum of two number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	
+	int a,b,r;
+	printf("enter a:");
+	scanf("%d",&a);
+	printf("enter b:");
+	scanf("%d",&b);
+	r = a+b;
+	printf("r=%d",r);
+	
+}
